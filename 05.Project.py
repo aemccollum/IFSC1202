015c8e70593b7d3b449d4c3dc3093c15bd975075
aemccollum <142817830+aemccollum@users.noreply.github.com>
2023-09-20 21:02:07 +0000

    enter

--- 05.Project.py
+++ 05.Project.py
@@ -1,9 +1,11 @@
 s_range = int(input("Enter Start of Range: "))
 e_range = int(input("Enter End of Range: ")) 
 print("Special Numbers between", s_range, "and", e_range)
-n = s_range + 1
 i = 0
+num = i
 while i <= 10000:
     i = i // 10
-    print(i)
+    power = i
     i += 1 
+digits**power
+print(i)
