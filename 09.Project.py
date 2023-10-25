67f3e173def8e6d8ae786dd5e09edce040d201c8
aemccollum <142817830+aemccollum@users.noreply.github.com>
2023-10-25 17:39:44 +0000

    Project 9

--- 09.Project.py
+++ 09.Project.py
@@ -2,7 +2,7 @@ a = []
 inputfile = open("09.Project Distances.csv")
 x = inputfile.readline()
 while x != "":
-	y = x.split(" ")
+	y = x.split(",")
 	for i in range(len(y)):
 		y[i] = (y[i])
 	a.append(y)
@@ -10,7 +10,12 @@ while x != "":
 for i in range(len(a)):
     for j in range(len(a[i])):
         print(a[i][j], end=' ')
-    
+for i in range(len(a[0])):
+	if from_city != a[0]:
+    	print("Invalid From City")
+for j in range (len(a[0])):
+	if to_city != a[0]:
+    	print("Invalid To City")   
 from_city = input("Enter From City: ")
 to_city = input("Enter To City: ")
 print(from_city,"to",to_city,"-")
